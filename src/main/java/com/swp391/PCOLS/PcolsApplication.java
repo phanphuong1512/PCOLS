a74fef4c872dbc2d626e9258dd86fc2903e98457
Koby <phuongandphan@gmail.com>
2025-01-21 12:10:24 +0700

    init Project
    Config and init Project include:
     - application.yml(connect db)
     - gradle.build(add dependencies)

--- src/main/java/com/swp391/PCOLS/PcolsApplication.java
+++ src/main/java/com/swp391/PCOLS/PcolsApplication.java
@@ -8,7 +8,6 @@ public class PcolsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PcolsApplication.class, args);
-		System.out.println("Hello World");
+		System.out.println("Hello World fuck you");
 	}
-
 }
